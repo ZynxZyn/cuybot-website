e2dc7f2e04293cd94f97d472ed0f7b3ff3aeebaa
zynxyz <nazihanf132@gmail.com>
2022-08-25 13:38:01 +0700

    Added Features Section

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             <ParticlesBackground/>
             <Navbar />
             <Home />
-            <div className="test"></div>
+            <Features />
         </div>
 
     );
--- src/Components/Section/Features.js
+++ src/Components/Section/Features.js
@@ -1,11 +1,75 @@
 import './Features.css';
+import gameSVG from '../Svg/gamepad-solid.svg';
+import generalSVG from '../Svg/gear-solid.svg';
+import moderationSVG from '../Svg/eye-solid.svg';
+import funSVG from '../Svg/face-laugh-beam-solid.svg';
 
 function Features(){
     return (
         <div className="Features">
-            <div className="features-content">
-                <h1>Hello</h1>
-            </div>
+            <section id='features'>
+                <div className="features-content">
+                    <h1 className='features-title'>Features<br></br>_____________</h1>
+                    <ul class="cards">
+                    <li>
+                        <a class="card">
+                            <img src={gameSVG} class="card__image" alt="" />
+                            <div class="card__overlay">
+                                <div class="card__header">                   
+                                    <img class="card__thumb" src="../logo.png" alt="" />
+                                    <div class="card__header-text">
+                                        <h3 class="card__title">Game</h3>            
+                                    </div>
+                                </div>
+                                <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>      
+                    </li>
+                    <li>
+                        <a class="card">
+                            <img src={generalSVG} class="card__image" alt="" />
+                            <div class="card__overlay">        
+                                <div class="card__header">              
+                                    <img class="card__thumb" src="../logo.png" alt="" />
+                                    <div class="card__header-text">
+                                        <h3 class="card__title">General</h3>
+                                    </div>
+                                </div>
+                                <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                    <a class="card">
+                        <img src={funSVG} class="card__image" alt="" />
+                        <div class="card__overlay">
+                            <div class="card__header">             
+                                <img class="card__thumb" src="../logo.png" alt="" />
+                                <div class="card__header-text">
+                                    <h3 class="card__title">Fun</h3>
+                                </div>          
+                            </div>
+                            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                        </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="card">
+                        <img src={moderationSVG} class="card__image" alt="" />
+                        <div class="card__overlay">
+                            <div class="card__header">               
+                                <img class="card__thumb" src="../logo.png" alt="" />
+                                <div class="card__header-text">
+                                    <h3 class="card__title">Moderation</h3>
+                                </div>          
+                            </div>
+                            <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                        </div>
+                        </a>
+                    </li>    
+                    </ul>
+                </div>
+            </section>
         </div>
     )
 }
